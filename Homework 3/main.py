f5c46474f4daf86f5a4fa0767c70a6c8680fbc0f
Rusu Vlad <96465609+vladrusu7@users.noreply.github.com>
2023-11-06 21:48:35 +0200

    minimax desene animate

--- Homework 3/main.py
+++ Homework 3/main.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 class NumberScrabble:
     def __init__(self):
         self.state = [0] * 10   # chosen numbers
@@ -45,7 +47,52 @@ class NumberScrabble:
     
     def is_draw(self):
         return self.total_moves == 9
-
+    
+def calculate_heuristic(game):
+    score = 0
+    match (game.turn):
+            case 0:
+                playerToCheck = game.player_a
+            case 1:
+                playerToCheck = game.player_b
+    
+    for i in range(len(playerToCheck)):
+        for j in range(i+1, len(playerToCheck)):
+            for k in range(j+1, len(playerToCheck)):
+                if playerToCheck[i] + playerToCheck[j] + playerToCheck[k] == 15:
+                    if game.turn == 0:
+                        score += 1
+                    else:
+                        score -= 1
+                        
+    print(f"score: {score}")
+    return score
+            
+def minmax(game, depth, player):
+    if depth == 0 or game.is_win():
+        return calculate_heuristic(game)
+    
+    match (player):
+        case 0:
+            min_value = float('inf')
+            for move in game.get_possible_moves():
+                new_game_state = deepcopy(game)
+                new_game_state.turn = 1
+                new_game_state.make_move(move)
+                value = minmax(new_game_state, depth - 1, 1)
+                min_value = min(min_value, value)
+            return min_value
+        
+        case 1:
+            best_value = -float('inf')
+            for move in game.get_possible_moves():
+                new_game_state = deepcopy(game)
+                new_game_state.turn = 0
+                new_game_state.make_move(move)
+                value = minmax(new_game_state, depth - 1, 0)
+                best_value = max(best_value, value)
+            return best_value
+    
 def main():
     game = NumberScrabble()
     
@@ -54,7 +101,24 @@ def main():
             case 0:
                 move = int(input(f"[A] choose a number: {game.get_possible_moves()} "))
             case 1:
-                move = int(input(f"[B] choose a number: {game.get_possible_moves()} "))
+                # move = int(input(f"[B] choose a number: {game.get_possible_moves()} "))
+                depth = min(len(game.get_possible_moves()), 2)
+                print(str(game.get_possible_moves()))
+                best_move = None
+                best_value = -float('inf')
+                
+                for move in game.get_possible_moves():
+                    new_game_state = deepcopy(game)
+                    new_game_state.make_move(move)
+                    value = minmax(new_game_state, depth, 1)
+                    # game.state[move] = 0
+                    
+                    if value > best_value:
+                        best_value = value
+                        best_move = move
+                        
+                move = best_move
+                print(move)
                 
         if game.make_move(move):
             if game.is_win():
